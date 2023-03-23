7bd0c0ab72ba365b7da1eb9d55a98f0824ce3233
Maicon Rauber <maicon.rauber@lojasrenner.com.br>
2023-03-23 17:46:50 -0300

    first commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.scss'
+
+class Botao extends React.Component {
+  render() {
+    // const isActive = false;
+    // const styles = {
+    //   backgroundColor: isActive ? "green" : "red",
+    // };
+    return <button className="botao">Botão</button>;
+  }
+}
+
+export default Botao;
